7bd71e4a48c476849f723a02477ab6896ddf73aa
2210943 <2210943@stud.hs-mannheim.de>
2024-11-14 01:12:57 +0100

    Implemented the hamburger menu and site routing

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:greendrop/src/common_widgets/dropdown.dart';
+import 'package:greendrop/src/pages/hamburger_menu.dart';
 import 'package:greendrop/src/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -13,99 +13,15 @@ class GreenDropApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AppTheme>(
-      create: (_) => AppTheme(),
-      builder: (context, _) => MaterialApp(
-        title: 'GreenDrop',
-        theme: ThemeData.from(colorScheme: AppTheme.lightTheme),
-        darkTheme: ThemeData.from(colorScheme: AppTheme.darkTheme),
-        themeMode: context.watch<AppTheme>().themeMode,
-        debugShowCheckedModeBanner: false,
-        home: const MyHomePage(title: 'GreenDrop'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final dynamic title;
-
-  const MyHomePage({super.key, required this.title});
-
-
-  List<DropdownMenuItem<String>> get dropdownMenuItems {
-    return [
-      const DropdownMenuItem<String>(key: Key("theme_1"), value: "dark", child: Text("Dark")),
-      const DropdownMenuItem<String>(key: Key("theme_2"), value: "light", child: Text("Light")),
-      const DropdownMenuItem<String>(key: Key("theme_3"), value: "system", child: Text("System")),
-    ];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(bottom: 8.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 200,
-                  child: Card(
-                    child: Center(child: Text("Card")),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: FilledButton(
-                    onPressed: () => print("hello"),
-                    child: const Text("Button")
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Consumer<AppTheme>(
-                  builder: (context, appTheme, child) {
-                    return CustomDropdownButton(
-                        items: dropdownMenuItems,
-                        value: appTheme.themeMode.name,
-                        onChanged: (s) {
-                          switch (s) {
-                            case "dark": {
-                              appTheme.themeMode = ThemeMode.dark;
-                            }
-                            case "light": {
-                              appTheme.themeMode = ThemeMode.light;
-                            }
-                            case "system": {
-                              appTheme.themeMode = ThemeMode.system;
-                            }
-                          }
-                        });
-                  },
-                ),
-              )
-            ],
-          )
+        create: (_) => AppTheme(),
+        builder: (context, _) => MaterialApp(
+          title: 'GreenDrop',
+          theme: ThemeData.from(colorScheme: AppTheme.lightTheme),
+          darkTheme: ThemeData.from(colorScheme: AppTheme.darkTheme),
+          themeMode: context.watch<AppTheme>().themeMode,
+          debugShowCheckedModeBanner: false,
+          home: const HamburgerMenu(),
         ),
-      ),
-      floatingActionButton: Consumer<AppTheme>(
-        builder: (context, appTheme, child) {
-          return FloatingActionButton(
-            onPressed: () {
-              appTheme.themeMode = appTheme.themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          );
-        },
-      )
-    );
-  }
+      );
+    }
 }
---
+++ frontend/lib/src/pages/hamburger_menu.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:greendrop/src/common_widgets/dropdown.dart';
+import 'package:greendrop/src/pages/settings.dart';
+
+import 'account.dart';
+import 'info_page.dart';
+import 'landing_page.dart';
+import 'orders.dart';
+
+class HamburgerMenu extends StatefulWidget {
+  const HamburgerMenu({super.key});
+
+  @override
+  State<HamburgerMenu> createState() => _HamburgerMenuState();
+}
+
+class _HamburgerMenuState extends State<HamburgerMenu> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String activePage = "Startseite";
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final bool isLargeScreen = width > 800;
+
+    return Theme(
+      data: ThemeData.dark(),
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          titleSpacing: 0,
+          leading: isLargeScreen
+              ? null
+              : IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  "Greendrob",
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                    onPressed: () => setState(() {
+                      activePage = "Account";
+                    }),
+                    child: const Text("#2210943")),
+                if (isLargeScreen) Expanded(child: _navBarItems())
+              ],
+            ),
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Icon(Icons.logo_dev)),
+            )
+          ],
+        ),
+        drawer: isLargeScreen ? null : _drawer(),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: activeScreen(),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _drawer() => Drawer(
+    child: ListView(
+      children: _menuItems
+          .map((item) => ListTile(
+        onTap: () {
+          setState(() {
+            activePage = item; // Update activePage with selected item
+          });
+          _scaffoldKey.currentState?.openEndDrawer();
+        },
+        title: Text(item),
+      ))
+          .toList(),
+    ),
+  );
+
+  Widget _navBarItems() => Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: _menuItems
+        .map((item) => InkWell(
+      onTap: () {
+        setState(() {
+          activePage = item;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            vertical: 24.0, horizontal: 16),
+        child: Text(
+          item,
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+    ))
+        .toList(),
+  );
+
+  final List<String> _menuItems = <String>[
+    'Startseite',
+    'Bestellungen',
+    'Account',
+    'Einstellungen',
+    'Impressum',
+  ];
+
+  Widget activeScreen() {
+    switch (activePage) {
+      case 'Startseite':
+        return LandingPage();
+      case 'Bestellungen':
+        return Orders();
+      case 'Account':
+        return Account();
+      case 'Einstellungen':
+        return Settings();
+      case 'Impressum':
+        return InfoPage();
+      default:
+        return const Text("Unknown Page");
+    }
+  }
+}
