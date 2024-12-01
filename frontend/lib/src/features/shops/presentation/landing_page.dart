69e3b33e861a3e7744730dc43026131a8be7bd2a
Sajid Shaukat <sajid1998.shau@gmail.com>
2024-12-01 19:36:09 +0100

    Add SingleScrollView in body

--- frontend/lib/src/features/shops/presentation/landing_page.dart
+++ frontend/lib/src/features/shops/presentation/landing_page.dart
@@ -19,76 +19,71 @@ class LandingPage extends StatelessWidget {
             floating: false,
             pinned: true,
             stretch: true,
+            leading: Container(),
             flexibleSpace: FlexibleSpaceBar(
                 centerTitle: true,
                 collapseMode: CollapseMode.parallax,
-                title: const Text("Collapsing Appbar",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    )),
+
                 background: ShopsMap(),
             ),
           ),
         ];
       },
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                const Expanded(child: ShopSearchBar()),
-                const SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: () => print("filter"),
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12.0),
-                    child: Icon(Icons.filter_alt),
-                  ),
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const Expanded(child: ShopSearchBar()),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () => print("filter"),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12.0),
+                      child: Icon(Icons.filter_alt),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 16.0),
-            child: Text(
-              "Coffeeshops in der Nähe",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            const Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: Text(
+                "Coffeeshops in der Nähe",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          ChangeNotifierProvider<ShopDataProvider>(
-            create: (context) => ShopDataProvider(),
-            child: Consumer<ShopDataProvider>(
-              builder: (context, provider, child) {
-                // Check if shopList is empty
-                if (provider.shopList.isEmpty) {
-                  return const Center(child: Text("No shops available."));
-                }
-
-                return ListView.separated(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  padding: const EdgeInsets.all(16),
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 16);
-                  },
-                  itemCount: provider.shopList.length,
-                  itemBuilder: (BuildContext context, index) {
-                    // Assuming shopList is a Map or List
-                    final shop = provider.shopList.values.toList()[index];
-                    return ShopCard(shop: shop);
-                  },
-                );
-              },
+            ChangeNotifierProvider<ShopDataProvider>(
+              create: (context) => ShopDataProvider(),
+              child: Consumer<ShopDataProvider>(
+                builder: (context, provider, child) {
+                  if (provider.shopList.isEmpty) {
+                    return const Center(child: Text("No shops available."));
+                  }
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(16),
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemCount: provider.shopList.length,
+                    itemBuilder: (BuildContext context, index) {
+                      final shop = provider.shopList.values.toList()[index];
+                      return ShopCard(shop: shop);
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
