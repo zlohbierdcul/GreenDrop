cbf1df1f08a55273ce17ca8d8b9f16207a9237d2
Mela1906 <melaniekollinger@gmail.com>
2024-12-02 22:46:52 +0100

    add orders page and orders detailed page, instead of getting data from provider, given examples

---
+++ frontend/lib/src/features/orders/presentation/order_details_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'orders.dart';
+
+//import '../features/orders/presentation/order_provider.dart';
+
+class OrderDetailsPage extends StatelessWidget {
+  final Order order;
+
+  const OrderDetailsPage({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Details Bestellung vom ${order.date}"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.asset(
+              order.shopImage,
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+
+            const SizedBox(height: 10),
+
+            Text(
+              order.shopName,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Text(
+              "Bestellnummer: ${order.date}", //TODO: Bestellnummer ändern
+              style: const TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Sie haben folgende Artikel bestellt:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: order.items.length,
+                itemBuilder: (context, index) {
+                  final item = order.items[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      children: [
+
+                        Text(
+                          '${item.number}.',
+                          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+
+                        const SizedBox(width: 10),
+
+                        Expanded(
+                          child: Text(
+                            item.name,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ),
+
+                        Text(
+                          '\$${item.price.toStringAsFixed(2)}',
+                          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text(
+              "Bezahlter Gesamtbetrag: €${order.totalAmount.toStringAsFixed(2)}",
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Zurück zur Bestellliste
+              },
+              child: const Text("Zurück"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- frontend/lib/src/features/orders/presentation/order_provider.dart
+++ frontend/lib/src/features/orders/presentation/order_provider.dart
@@ -1,35 +1,21 @@
-import 'package:flutter/material.dart';
-
-// Bestellung Modell
-class Order {
-  final String shopName;
-  final String date;
-  final int itemCount;
-  final double totalAmount;
-
-  Order({
-    required this.shopName,
-    required this.date,
-    required this.itemCount,
-    required this.totalAmount,
-  });
-}
-
-class OrderProvider with ChangeNotifier {
-  //TODO: mit provider verbinden
-  final List<Order> _orders = []; 
-
-  List<Order> get orders => _orders;
+// import 'package:flutter/material.dart';
 
 
-  void addOrder(Order order) {
-    _orders.add(order);
-    notifyListeners(); 
-  }
+// class OrderProvider with ChangeNotifier {
+//   //TODO: mit provider verbinden
+//   final List<Order> _orders = []; 
 
-  void setOrders(List<Order> newOrders) {
-    _orders.clear();
-    _orders.addAll(newOrders);
-    notifyListeners(); 
-  }
-}
+//   List<Order> get orders => _orders;
+
+
+//   void addOrder(Order order) {
+//     _orders.add(order);
+//     notifyListeners(); 
+//   }
+
+//   void setOrders(List<Order> newOrders) {
+//     _orders.clear();
+//     _orders.addAll(newOrders);
+//     notifyListeners(); 
+//   }
+// }
