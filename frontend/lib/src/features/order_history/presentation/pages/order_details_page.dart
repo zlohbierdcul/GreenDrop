409b9e91d8cc8061fa95d3f63861d1e67b5a2209
CedricBienert <cedric.bienert@outlook.de>
2024-12-03 20:20:02 +0100

    Seiten der alten Bestellungen hübscher gemacht

--- frontend/lib/src/features/order_history/presentation/pages/order_details_page.dart
+++ frontend/lib/src/features/order_history/presentation/pages/order_details_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../../hamburger_menu/presentation/hamburger_menu.dart';
 import 'orders_page.dart';
-
-//import '../features/orders/presentation/order_provider.dart';
 
 class OrderDetailsPage extends StatelessWidget {
   final Order order;
@@ -11,102 +10,127 @@ class OrderDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Details Bestellung vom ${order.date}"),
-      ),
-      body: SingleChildScrollView(
+      appBar: AppDrawer.buildGreendropsAppBar(),
+      body: Center(
         child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              order.shopImage,
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              order.shopName,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Bestellnummer: ${order.date}", //TODO: Bestellnummer ändern
-              style: const TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "Sie haben folgende Artikel bestellt:",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true, // Hinzugefügt, um die Liste auf die Größe des Inhalts zu beschränken
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: order.items.length,
-              itemBuilder: (context, index) {
-                final item = order.items[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        '${item.number}.',
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: Card(
+                    child: Center(
+                      child: Text(
+                        "Details Bestellung vom ${order.date}",
                         style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Text(
-                          item.name,
-                          style: const TextStyle(fontSize: 16),
+                          fontSize: 24,
                         ),
                       ),
-                      Text(
-                        "€${item.price.toStringAsFixed(2)}",
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(16.0), // Setzt die Rundung der Ecken
+                            child: Image.asset(
+                              order.shopImage,
+                              width: double.infinity,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            order.shopName,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            "Bestellnummer: ${order.date}", //TODO: Bestellnummer ändern
+                            style: const TextStyle(fontSize: 16, color: Colors.grey),
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Sie haben folgende Artikel bestellt:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          ListView.builder(
+                            shrinkWrap: true, // Hinzugefügt, um die Liste auf die Größe des Inhalts zu beschränken
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: order.items.length,
+                            itemBuilder: (context, index) {
+                              final item = order.items[index];
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      '${item.number}.',
+                                      style: const TextStyle(
+                                          fontSize: 16, fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Expanded(
+                                      child: Text(
+                                        item.name,
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                    Text(
+                                      "€${item.price.toStringAsFixed(2)}",
+                                      style: const TextStyle(
+                                          fontSize: 16, fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(), // Leerer Container für Platzierung
+                              ),
+                              Text(
+                                "Bezahlter Gesamtbetrag: €${order.totalAmount.toStringAsFixed(2)}",
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                );
-              },
-            ),
-            
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(), // Leerer Container für Platzierung
                 ),
-                Text(
-                  "Bezahlter Gesamtbetrag: €${order.totalAmount.toStringAsFixed(2)}",
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.right,
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Zurück zur Bestellliste
-              },
-              child: const Text("Zurück"),
-            )
-          ],
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
