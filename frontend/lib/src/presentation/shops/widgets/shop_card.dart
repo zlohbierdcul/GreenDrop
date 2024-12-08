f9f8428547bf432725553c1613a091fd31d35ead
Luc Dreibholz <luc.dreibholz@gmx.net>
2024-12-08 17:02:16 +0100

    updated home page

--- frontend/lib/src/presentation/shops/widgets/shop_card.dart
+++ frontend/lib/src/presentation/shops/widgets/shop_card.dart
@@ -22,9 +22,6 @@ class ShopCard extends StatelessWidget {
         cursor: SystemMouseCursors.click,
         child: GestureDetector(
           onTap: () {
-            productProvider.clearProducts();
-            productProvider.loadShopProducts(shop);
-            cartProvider.resetCart();
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => ShopPage(shop: shop)));
           },
