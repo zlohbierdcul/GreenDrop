1a56d05e12033e294975b58b20a7865f9affbf6d
Mela1906 <melaniekollinger@gmail.com>
2024-12-18 19:49:36 +0100

    Feature/bestellungen (#64)
    * change name
    
    * change provider
    
    * getUserOrder()
    
    * implement getUserOrders in strapi_order_repository.dart
    
    * neuere APG version gebraucht
    
    * add OrderHistoryProvider in main
    
    * tried to fix getUserOrders
    
    * some fixes
    
    ---------
    
    Co-authored-by: Luc Dreibholz <luc.dreibholz@gmx.net>

--- frontend/lib/src/domain/models/order.dart
+++ frontend/lib/src/domain/models/order.dart
@@ -8,54 +8,52 @@ import 'package:greendrop/src/domain/models/user.dart';
 class Order {
   final String? id;
   final String status;
-  final User user;
+  final User? user;
   final Shop shop;
   final Address address;
   final String paymentMethod;
   final List<OrderItem>? orderItems;
 
-  Order({
-    this.id,
-    required this.status,
-    required this.user,
-    required this.shop,
-    required this.address,
-    required this.paymentMethod,
-    this.orderItems
-  });
+  Order(
+      {this.id,
+      required this.status,
+      this.user,
+      required this.shop,
+      required this.address,
+      required this.paymentMethod,
+      this.orderItems});
 
   // Factory constructor to create an Order object from a JSON entry
-  static Future<Order> fromJson(String id, Map<String, dynamic> json) async {
+  static Future<Order> fromJson(Map<String, dynamic> json) async {
     return Order(
-      id: id,
-      status: json['status'],
-      user: json['user']['id'],
-      shop: json['shop']['id'],
-      address: json['address']['id'],
-      paymentMethod: json['paymentMethod'],
-    );
+        id: json['id'].toString(),
+        status: json['state'],
+        shop: await Shop.fromJson(json['shop']),
+        address: Address.fromJson(json['user_address']),
+        paymentMethod: json['payment_method']);
   }
 
   Map<String, dynamic> toJson() {
-    final totalPrice = orderItems?.map((o) => o.totalAmount).toList().reduce((a, b) => a + b);
+    final totalPrice =
+        orderItems?.map((o) => o.totalAmount).toList().reduce((a, b) => a + b);
 
-  return {
-    'id': id,
-    'state': status,
-    'user': user.id,
-    'shop': shop.id,
-    'user_address': address.id,
-    'payment_method': paymentMethod,
-    'total_price': totalPrice,
-    'items': orderItems?.map((item) => item.toJson()).toList(),
-  };
-}
+    return {
+      'id': id,
+      'state': status,
+      'user': user?.id,
+      'shop': shop.id,
+      'user_address': address.id,
+      'payment_method': paymentMethod,
+      'total_price': totalPrice,
+      'items': orderItems?.map((item) => item.toJson()).toList(),
+    };
+  }
 
   // Static method to parse mock data and create a list of Orders
   static List<Future<Order>> parseOrders(String jsonData) {
     final Map<String, dynamic> data = json.decode(jsonData);
     return data.entries
-        .map((entry) async => await Order.fromJson(entry.key, entry.value))
+        .map((entry) async => await Order.fromJson(entry.value))
         .toList();
   }
 
