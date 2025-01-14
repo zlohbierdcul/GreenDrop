74a24bd1a6afb55d743de3cc5f5d7a1fa65f1cb0
SajidShaukat <88154713+SajidShaukat@users.noreply.github.com>
2025-01-14 11:22:33 +0100

    change order categor (#112)
    Co-authored-by: Luc Dreibholz <luc.dreibholz@gmx.net>

--- frontend/lib/src/presentation/products/provider/product_provider.dart
+++ frontend/lib/src/presentation/products/provider/product_provider.dart
@@ -1,4 +1,5 @@
-import "package:collection/collection.dart";
+import 'dart:collection'; // Für LinkedHashMap
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:greendrop/src/data/repositories/interfaces/shop_repository.dart';
 import 'package:greendrop/src/data/repositories/strapi/strapi_shop_repository.dart';
@@ -7,20 +8,23 @@ import 'package:greendrop/src/domain/models/shop.dart';
 
 class ProductProvider extends ChangeNotifier {
   IShopRepository repository = StrapiShopRepository();
-  
-  Map<String, List<Product>> _productMap = {};
+
+
+  LinkedHashMap<String, List<Product>> _productMap = LinkedHashMap();
   bool _isLoading = true;
 
-  Map<String, List<Product>> get productMap => _productMap;
+  LinkedHashMap<String, List<Product>> get productMap => _productMap;
   bool get isLoading => _isLoading;
 
   void clearProducts() {
-    _productMap = {};
+    _productMap.clear();
     notifyListeners();
   }
 
   void setShopProducts(List<Product> products) {
-    _productMap = groupBy(products, (p) => p.category);
+    _productMap = LinkedHashMap.from(
+      groupBy(products, (p) => p.category),
+    );
     notifyListeners();
   }
 
@@ -28,7 +32,13 @@ class ProductProvider extends ChangeNotifier {
     _isLoading = true;
     notifyListeners();
     List<Product> products = await repository.getAllShopProducts(shop.id);
-    _productMap = groupBy(products, (p) => p.category);
+    var groupedProducts = groupBy(products, (p) => p.category);
+    const categoryOrder = ['Rauchbar', 'Essbar', 'Zubehör'];
+    _productMap = LinkedHashMap.fromEntries(
+      categoryOrder
+          .where((category) => groupedProducts.containsKey(category)) // Nur vorhandene Kategorien
+          .map((category) => MapEntry(category, groupedProducts[category]!)),
+    );
     _isLoading = false;
     notifyListeners();
   }
